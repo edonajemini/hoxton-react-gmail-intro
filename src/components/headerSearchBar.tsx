6f74b6eaf22afb679167ffcb2bf25ea137605b15
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-07-26 01:57:53 +0200

    header Done

---
+++ src/components/headerSearchBar.tsx
@@ -0,0 +1,20 @@
+
+ /*function header(){
+  return(
+    <header className="header">
+    <div className="search">
+      <input className="search-bar" placeholder="Search mail" />
+    </div>
+  </header>
+  );
+ }
+*/
+function headerSearchBar(){
+    return(
+        <div className="search">
+        <input className="search-bar" placeholder="Search mail" />
+      </div> 
+    );
+}
+
+export default headerSearchBar;
